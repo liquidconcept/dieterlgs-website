abe2aa6a3b0e986b37321d8064dbbc3946d1705f
Simon Walther <simon.walther@liquid-concept.ch>
2013-05-14 15:27:15 +0200

    finish two form with validation and mailer, finish global design

---
+++ config/application.rb
@@ -0,0 +1,7 @@
+# encoding: utf-8
+
+#
+# COMMAND
+#
+COMMAND_EMAIL_TO = 'simon.walther@liquid-concept.ch'
+COMMAND_SUBJECT  = "demande d'offre depuis le formulaire du site"
---
+++ content/assets/javascripts/application.js
@@ -0,0 +1,9 @@
+// =require modernizr
+// =require jquery
+// =require jquery-ui
+// =require jquery-h5validate
+
+// validation init
+$(function() {
+  $('form').h5Validate();
+});
