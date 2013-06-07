5bfd71c0fc1fcd7ea22bef2ab5966ec1e38d7e41
Ludovic Turmel <ludovic.turmel@liquid-concept.ch>
2013-06-07 11:32:16 +0200

    Select the right range of date for lesson package

--- content/assets/javascripts/application.js
+++ content/assets/javascripts/application.js
@@ -3,7 +3,41 @@
 // =require jquery-ui
 // =require jquery-h5validate
 
-// validation init
 $(function() {
+  // validation form init
   $('form').h5Validate();
+
+  // pickup the right date for the package
+  $('#pack_checkbox > .checkbox > input').on('click', function() {
+    if ($('#six_hours_pack').is(':checked')) {
+      $('.bigRow > #six_hours_date').show();
+      $('.bigRow > #eight_hours_date').hide();
+      $('.bigRow > #twleve_hours_date').hide();
+    } else if ($('#eight_hours_pack').is(':checked')) {
+      $('.bigRow > #eight_hours_date').show();
+      $('.bigRow > #six_hours_date').hide();
+      $('.bigRow > #twelve_hours_date').hide();
+    } else {
+      $('.bigRow > #twelve_hours_date').show();
+      $('.bigRow > #six_hours_date').hide();
+      $('.bigRow > #eight_hours_date').hide();
+    }
+  });
+
+  // pickup the right date for the package
+  $('#pack_checkbox_2 > .checkbox > input').on('click', function() {
+    if ($('#tech').is(':checked')) {
+      $('.bigRow > #tech_date').show();
+      $('.bigRow > #flow_date').hide();
+      $('.bigRow > #both_date').hide();
+    } else if ($('#flow').is(':checked')) {
+      $('.bigRow > #flow_date').show();
+      $('.bigRow > #tech_date').hide();
+      $('.bigRow > #both_date').hide();
+    } else {
+      $('.bigRow > #both_date').show();
+      $('.bigRow > #tech_date').hide();
+      $('.bigRow > #flow_date').hide();
+    }
+  });
 });
