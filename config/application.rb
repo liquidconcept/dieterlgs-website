d8d05bbc083163bdfdb334086214fb44274845c5
Néville Dubuis <neville.dubuis@liquid-concept.ch>
2013-07-01 09:45:21 +0200

    send mail to multiple target

--- config/application.rb
+++ config/application.rb
@@ -3,5 +3,5 @@
 #
 # COMMAND
 #
-COMMAND_EMAIL_TO = 'didischmid@bluewin.ch'
+COMMAND_EMAIL_TO = ['didischmid@bluewin.ch', 'info@liquid-concept.ch']
 COMMAND_SUBJECT  = "demande d'offre depuis le formulaire du site"
